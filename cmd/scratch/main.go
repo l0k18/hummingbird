ad477c6990aa9b47bbb02277e117f1566b060aeb
Loki Verloren <l0k1@null.net>
2018-03-12 12:07:01 +0200

    minor changes

--- cmd/scratch/main.go
+++ cmd/scratch/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/l0k1verloren/go-wbbst/pkg/tree"
+	"github.com/l0k1verloren/go-wbbst/pkg/tree32"
 )
 
 func main() {
-	tree := tree.NewTree()
-	fmt.Println(tree.Store)
+	tree := tree32.NewTree()
+	fmt.Println(tree)
+	tree.AddRow()
+	fmt.Println(tree)
+	tree.AddRow()
+	fmt.Println(tree)
+	tree.AddRow()
+	fmt.Println(tree)
+
 }
